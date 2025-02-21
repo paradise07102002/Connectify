70d4757ffc04dfbff3798bfb2246d09f9dbf5f2c
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-02-21 16:38:41 +0700

    Add function to check empty text field

--- lib/screens/register/register_screen.dart
+++ lib/screens/register/register_screen.dart
@@ -16,6 +16,36 @@ class _RegisterScreen extends State<RegisterScreen> {
 
   String? _nameError, _emailError, _passwordError, _confirmPasswordError;
 
+  void _register() async {
+    setState(() {
+      _nameError =
+          _nameController.text.trim().isEmpty ? "Please enter name" : null;
+      _emailError =
+          _emailController.text.trim().isEmpty ? "Please enter email" : null;
+      _passwordError =
+          _passwordController.text.trim().isEmpty
+              ? "Please enter password"
+              : null;
+      _confirmPasswordError =
+          _confirmPasswordController.text.trim().isEmpty
+              ? "Please confirm your password"
+              : null;
+    });
+
+    if (_nameController.text.trim().isEmpty ||
+        _emailController.text.trim().isEmpty ||
+        _passwordController.text.trim().isEmpty ||
+        _confirmPasswordController.text.trim().isEmpty) {
+      _showFieldErrors();
+      return;
+    }
+  }
+
+  //The function updates again to display the error
+  void _showFieldErrors() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,24 +67,43 @@ class _RegisterScreen extends State<RegisterScreen> {
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            CustomTextField(label: 'Your Name', icon: Icons.person, controller: _nameController, errorText: _nameError),
-            CustomTextField(label: 'Email', icon: Icons.email, controller: _emailController, errorText: _emailError),
-            CustomTextField(label: 'Your Name', icon: Icons.lock, controller: _passwordController, errorText: _passwordError, isPassword: true),
-            CustomTextField(label: 'Your Name', icon: Icons.person, controller: _confirmPasswordController, errorText: _confirmPasswordError, isPassword: true),
+            CustomTextField(
+              label: 'Your Name',
+              icon: Icons.person,
+              controller: _nameController,
+              errorText: _nameError,
+            ),
+            CustomTextField(
+              label: 'Email',
+              icon: Icons.email,
+              controller: _emailController,
+              errorText: _emailError,
+            ),
+            CustomTextField(
+              label: 'Your Name',
+              icon: Icons.lock,
+              controller: _passwordController,
+              errorText: _passwordError,
+              isPassword: true,
+            ),
+            CustomTextField(
+              label: 'Your Name',
+              icon: Icons.person,
+              controller: _confirmPasswordController,
+              errorText: _confirmPasswordError,
+              isPassword: true,
+            ),
 
             SizedBox(height: 10),
-            
-            PrimaryButton(text: 'Sign up', onPressed: () {}),
+
+            PrimaryButton(text: 'Sign up', onPressed: _register),
             SizedBox(height: 20),
             Center(
               child: TextButton(
-                onPressed: () {
-                  
-                },
+                onPressed: () {},
                 child: Text("Already have an account? Sign in"),
               ),
             ),
-
           ],
         ),
       ),
