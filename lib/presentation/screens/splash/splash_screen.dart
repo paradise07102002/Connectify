565eb15d53bc8a74f70a304996c6b8dbbc2debef
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-02-24 17:45:57 +0700

    Restructure the project directory

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,6 +1,6 @@
-import 'package:connectify/screens/home/home_screen.dart';
+import 'package:connectify/presentation/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:connectify/screens/welcome/welcome_screen.dart';
+import 'package:connectify/presentation/screens/welcome/welcome_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
