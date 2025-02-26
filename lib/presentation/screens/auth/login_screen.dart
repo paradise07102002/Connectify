f9fb13d13f3aef0860fdafa2bcc27deb82138863
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-02-26 22:38:08 +0700

    update LoginScreen

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:connectify/domain/controllers/auth/login_controller.dart';
 import 'package:connectify/presentation/widgets/buttons/primary_button.dart';
 import 'package:connectify/presentation/widgets/fields/custom_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -8,8 +10,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final loginController = Provider.of<LoginController>(context);
+
     return Scaffold(
       appBar: AppBar(title: Text("Login"), centerTitle: true),
       body: Padding(
@@ -19,21 +26,50 @@ class _LoginScreen extends State<LoginScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: 30),
-            Center(child: Image.asset('assets/images/logo_connectify.png', width: 300)),
+            Center(
+              child: Image.asset(
+                'assets/images/logo_connectify.png',
+                width: 300,
+              ),
+            ),
             SizedBox(height: 16),
-            CustomTextField(label: "Email", icon: Icons.email),
+            CustomTextField(
+              label: "Email",
+              icon: Icons.email,
+              controller: _emailController,
+              errorText: loginController.emailError,
+            ),
             SizedBox(height: 16),
-            CustomTextField(label: "Password", icon: Icons.lock, isPassword: true),
+            CustomTextField(
+              label: "Password",
+              icon: Icons.lock,
+              controller: _passwordController,
+              errorText: loginController.passwordError,
+              isPassword: true,
+            ),
             SizedBox(height: 24),
-            PrimaryButton(text: "Login", onPressed: () {}),
+
+            loginController.isLoading
+                ? Center(child: CircularProgressIndicator())
+                : PrimaryButton(
+                  text: "Login",
+                  onPressed: () async {
+                    await loginController.login(
+                      context: context,
+                      email: _emailController.text.trim(),
+                      password: _passwordController.text,
+                    );
+                  },
+                ),
+
             SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("Don't have an account?"),
-                TextButton(onPressed: () {}, child: Text("Sign up"))
+                TextButton(onPressed: () {}, child: Text("Sign up")),
               ],
-            )
+            ),
           ],
         ),
       ),
