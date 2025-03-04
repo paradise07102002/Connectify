a5383c042051b0727d2d96446dd6aa026572939f
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-04 19:43:08 +0700

    feat: Create Setting Screen

---
+++ lib/presentation/screens/settings/setting_screen.dart
@@ -0,0 +1,47 @@
+import 'package:connectify/presentation/widgets/settings/settings_item.dart';
+import 'package:connectify/presentation/widgets/settings/settings_section.dart';
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  @override
+  _SettingScreen createState() => _SettingScreen();
+}
+
+class _SettingScreen extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    // Get the screen size to make the UI responsive
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings'), centerTitle: true,),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: size.width * 0.15,
+                      backgroundImage: NetworkImage('https://static.wikia.nocookie.net/lookism/images/b/bb/525_Young_Shingen.png/revision/latest?cb=20241024142804'),
+                    ),
+                    Text('Phạm Đình Khôi', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text('pdk@gmail.com', style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.02),
+            
+            SettingsSection([
+              SettingsItem(Icons.person, 'Account & Profile')
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
