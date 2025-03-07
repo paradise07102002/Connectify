51b454f0f699e5182a951b07484bf7245a150c6f
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-07 22:08:43 +0700

    feat: Logout when the logout button is pressed

--- lib/presentation/screens/settings/setting_screen.dart
+++ lib/presentation/screens/settings/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectify/domain/controllers/logout_controller.dart';
 import 'package:connectify/presentation/widgets/settings/settings_item.dart';
 import 'package:connectify/presentation/widgets/settings/settings_section.dart';
 import 'package:flutter/material.dart';
@@ -8,14 +9,15 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreen extends State<SettingScreen> {
+  final LogoutController _logoutController = LogoutController();
+
   @override
   Widget build(BuildContext context) {
-
     // Get the screen size to make the UI responsive
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(title: Text('Settings'), centerTitle: true,),
+      appBar: AppBar(title: Text('Settings'), centerTitle: true),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -25,9 +27,17 @@ class _SettingScreen extends State<SettingScreen> {
                   children: [
                     CircleAvatar(
                       radius: size.width * 0.15,
-                      backgroundImage: NetworkImage('https://static.wikia.nocookie.net/lookism/images/b/bb/525_Young_Shingen.png/revision/latest?cb=20241024142804'),
+                      backgroundImage: NetworkImage(
+                        'https://static.wikia.nocookie.net/lookism/images/b/bb/525_Young_Shingen.png/revision/latest?cb=20241024142804',
+                      ),
                     ),
-                    Text('Phạm Đình Khôi', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text(
+                      'Phạm Đình Khôi',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                     Text('pdk@gmail.com', style: TextStyle(color: Colors.grey)),
                   ],
                 ),
@@ -35,9 +45,16 @@ class _SettingScreen extends State<SettingScreen> {
             ),
 
             SizedBox(height: size.height * 0.02),
-            
+
+            SettingsSection([SettingsItem(Icons.person, 'Account & Profile')]),
             SettingsSection([
-              SettingsItem(Icons.person, 'Account & Profile')
+              SettingsItem(
+                Icons.logout,
+                'Logout',
+                onTap: () async {
+                  await _logoutController.logout(context);
+                },
+              ),
             ]),
           ],
         ),
