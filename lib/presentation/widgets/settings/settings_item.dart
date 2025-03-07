03f9a9c713952abbbce0217b206cf8c1d34ecf81
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-07 21:48:44 +0700

    feat: Add onTap event to SettingItem

--- lib/presentation/widgets/settings/settings_item.dart
+++ lib/presentation/widgets/settings/settings_item.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 class SettingsItem extends StatelessWidget {
   final IconData icon;
   final String title;
-  SettingsItem(this.icon, this.title);
+  final VoidCallback? onTap;
+  
+  SettingsItem(this.icon, this.title, {this.onTap});
 
   @override
   Widget build(BuildContext) {
     return ListTile(
       leading: Icon(icon, color: Colors.blueGrey),
       title: Text(title),
-      onTap: () {},
+      onTap: onTap,
     );
   }
 }
