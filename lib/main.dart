30bc3d39da106f2999fd564dd0f4ddf2c832b5e9
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-09 18:09:06 +0700

    chore: Update library imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:connectify/core/utils/navigator_service.dart';
 import 'package:connectify/data/providers/user_provider.dart';
 import 'package:connectify/domain/controllers/check_field_controller.dart';
-import 'package:connectify/domain/controllers/login_controller.dart';
+import 'package:connectify/domain/controllers/auth/login_controller.dart';
 import 'package:connectify/domain/controllers/auth/signup_controller.dart';
 import 'package:connectify/presentation/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
