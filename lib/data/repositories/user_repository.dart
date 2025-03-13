89fb8030b96feb9e70d4daa94bd580282122f0e8
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-13 15:54:57 +0700

    feat: Add uploadAvatar method in UserRepository

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:connectify/data/models/user_model.dart';
+import 'package:connectify/data/services/auth_service.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:path/path.dart' as path;
 
 class UserRepository {
-  String baseUrl = "http://10.0.2.2:5151/api/User";
+  // String baseUrl = "http://10.0.2.2:5151/api/User";
+  String baseUrl = "http://192.168.1.96:5151/api/User";
   FlutterSecureStorage secureStorage = const FlutterSecureStorage();
 
   Future<UserModel?> fetchUserInfo(String accessToken) async {
@@ -23,5 +27,36 @@ class UserRepository {
       print("error: ${response.statusCode}");
       return null;
     }
+  }
+
+  Future<String?> uploadAvatar(File imageFile) async {
+    final AuthService _authService = AuthService();
+
+    try {
+      String? accessToken = await _authService.getAccessToken();
+
+      var request = http.MultipartRequest(
+        "POST",
+        Uri.parse("$baseUrl/upload-avatar"),
+      );
+      request.headers["Authorization"] = "Bearer $accessToken";
+
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'file',
+          imageFile.path,
+          filename: path.basename(imageFile.path),
+        ),
+      );
+
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        String responseBody = await response.stream.bytesToString();
+        return responseBody;
+      }
+    } catch (e) {
+      print("Image loading error: $e");
+    }
+    return null;
   }
 }
