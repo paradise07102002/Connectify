565e967f444e3f102b02c1210637d8d6a8e4b7f7
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-13 20:46:46 +0700

    Cleanup: Remove unused flutter/widgets.dart import

--- lib/core/utils/avatar_utils.dart
+++ lib/core/utils/avatar_utils.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:connectify/data/providers/user_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
