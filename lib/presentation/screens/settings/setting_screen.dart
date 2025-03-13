e43ae3be14c3a08eace405403867f14a3ede0937
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-13 15:59:07 +0700

    feat: SettingScreen - Show Options when clicking on the image avatar

--- lib/presentation/screens/settings/setting_screen.dart
+++ lib/presentation/screens/settings/setting_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connectify/core/utils/avatar_utils.dart';
 import 'package:connectify/data/providers/user_provider.dart';
 import 'package:connectify/data/services/auth_service.dart';
 import 'package:connectify/presentation/screens/welcome/welcome_screen.dart';
@@ -14,6 +15,9 @@ class SettingScreen extends StatefulWidget {
 class _SettingScreen extends State<SettingScreen> {
   final AuthService _authService = AuthService();
 
+  String avatarError =
+      "https://storage.googleapis.com/connectify-social-bucket/avatar/3b9ce54a-0e2a-456d-92c9-18eb86041078_f5e7ae7d-f344-4515-b2ce-2927808d1068_cancel-297373_1280.png";
+
   @override
   void initState() {
     super.initState();
@@ -28,66 +32,82 @@ class _SettingScreen extends State<SettingScreen> {
 
     return Scaffold(
       appBar: AppBar(title: Text('Settings'), centerTitle: true),
-      body: userProvider.isLoading
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: Center(
+      body:
+          userProvider.isLoading
+              ? Center(child: CircularProgressIndicator())
+              : SingleChildScrollView(
                 child: Column(
                   children: [
-                    CircleAvatar(
-                      radius: size.width * 0.15,
-                      backgroundImage: NetworkImage(
-                        userProvider.userModel?.avatarUrl ??
-                            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fvillagesonmacarthur.com%2Fcommunity%2Fblank-avatar%2F&psig=AOvVaw3hPIuKAniDndYtaNOV_Z9I&ust=1741603688296000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCO9r3p_IsDFQAAAAAdAAAAABAJ',
+                    Container(
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Consumer<UserProvider>(
+                              builder: (context, userProvider, child) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    AvatarUtils.showAvatarOptions(
+                                      context,
+                                      userProvider.userModel?.avatarUrl ?? avatarError,
+                                    );
+                                  },
+                                  child: CircleAvatar(
+                                    radius: size.width * 0.15,
+                                    backgroundImage: NetworkImage(
+                                      userProvider.userModel?.avatarUrl ?? avatarError,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                            Text(
+                              userProvider.userModel?.fullName ?? "",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              userProvider.userModel?.email ?? "",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    Text(
-                      userProvider.userModel?.fullName ?? "",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+
+                    SizedBox(height: size.height * 0.02),
+
+                    SettingsSection([
+                      SettingsItem(Icons.person, 'Account & Profile'),
+                    ]),
+                    SettingsSection([
+                      SettingsItem(Icons.lock, 'Change password'),
+                      SettingsItem(Icons.lock_clock, 'Privacy settings'),
+                      SettingsItem(Icons.notifications, 'Notification'),
+                      SettingsItem(Icons.language, 'Language'),
+                    ]),
+                    SettingsSection([
+                      SettingsItem(
+                        Icons.logout,
+                        'Logout',
+                        onTap: () async {
+                          bool success = await _authService.logout();
+                          if (success) {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => WelcomeScreen(),
+                              ),
+                              (route) => false,
+                            );
+                          }
+                        },
                       ),
-                    ),
-                    Text(
-                      userProvider.userModel?.email ?? "",
-                      style: TextStyle(color: Colors.grey),
-                    ),
+                    ]),
                   ],
                 ),
               ),
-            ),
-
-            SizedBox(height: size.height * 0.02),
-
-            SettingsSection([SettingsItem(Icons.person, 'Account & Profile')]),
-            SettingsSection([
-              SettingsItem(Icons.lock, 'Change password'),
-              SettingsItem(Icons.lock_clock, 'Privacy settings'),
-              SettingsItem(Icons.notifications, 'Notification'),
-              SettingsItem(Icons.language, 'Language'),
-            ]),
-            SettingsSection([
-              SettingsItem(
-                Icons.logout,
-                'Logout',
-                onTap: () async {
-                  bool success = await _authService.logout();
-                  if (success) {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => WelcomeScreen()),
-                      (route) => false,
-                    );
-                  }
-                },
-              ),
-            ]),
-          ],
-        ),
-      ),
     );
   }
 }
