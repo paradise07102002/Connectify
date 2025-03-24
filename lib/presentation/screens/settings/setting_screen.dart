0ac2f8154af0c4eb40e86ad521803001b669c466
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-24 20:41:21 +0700

    Refactor SettingScreen: Extract widgets (SettingList, SettingProfileHeader)

--- lib/presentation/screens/settings/setting_screen.dart
+++ lib/presentation/screens/settings/setting_screen.dart
@@ -1,9 +1,7 @@
-import 'package:connectify/core/utils/avatar_utils.dart';
 import 'package:connectify/data/providers/user_provider.dart';
 import 'package:connectify/data/services/auth_service.dart';
-import 'package:connectify/presentation/screens/welcome/welcome_screen.dart';
-import 'package:connectify/presentation/widgets/settings/settings_item.dart';
-import 'package:connectify/presentation/widgets/settings/settings_section.dart';
+import 'package:connectify/presentation/components/settings/settings_list.dart';
+import 'package:connectify/presentation/components/settings/settings_profile_header.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -28,88 +26,23 @@ class _SettingScreen extends State<SettingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Get the screen size to make the UI responsive
-    final size = MediaQuery.of(context).size;
-    final userProvider = Provider.of<UserProvider>(context);
-
     return Scaffold(
       appBar: AppBar(title: Text('Settings'), centerTitle: true),
-      body:
-          userProvider.isLoading
+      body: Consumer<UserProvider>(
+        builder: (context, userProvider, child) {
+          return userProvider.isLoading
               ? Center(child: CircularProgressIndicator())
               : SingleChildScrollView(
                 child: Column(
                   children: [
-                    Container(
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Consumer<UserProvider>(
-                              builder: (context, userProvider, child) {
-                                return GestureDetector(
-                                  onTap: () {
-                                    AvatarUtils.showAvatarOptions(
-                                      context,
-                                      userProvider.userModel?.avatarUrl ?? avatarError,
-                                    );
-                                  },
-                                  child: CircleAvatar(
-                                    radius: size.width * 0.15,
-                                    backgroundImage: NetworkImage(
-                                      userProvider.userModel?.avatarUrl ?? avatarError,
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                            Text(
-                              userProvider.userModel?.fullName ?? "",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              userProvider.userModel?.email ?? "",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: size.height * 0.02),
-
-                    SettingsSection([
-                      SettingsItem(Icons.person, 'Account & Profile'),
-                    ]),
-                    SettingsSection([
-                      SettingsItem(Icons.lock, 'Change password'),
-                      SettingsItem(Icons.lock_clock, 'Privacy settings'),
-                      SettingsItem(Icons.notifications, 'Notification'),
-                      SettingsItem(Icons.language, 'Language'),
-                    ]),
-                    SettingsSection([
-                      SettingsItem(
-                        Icons.logout,
-                        'Logout',
-                        onTap: () async {
-                          bool success = await _authService.logout();
-                          if (success) {
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => WelcomeScreen(),
-                              ),
-                              (route) => false,
-                            );
-                          }
-                        },
-                      ),
-                    ]),
+                    SettingsProfileHeader(),
+                    SizedBox(height: 20),
+                    SettingsList(authService: _authService),
                   ],
                 ),
-              ),
+              );
+        },
+      ),
     );
   }
 }
