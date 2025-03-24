5d1b993bda13e56d4f2ab02c71da7f396f671f31
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-24 13:27:40 +0700

    feat: Create PostPhotoGallary

---
+++ lib/presentation/components/post/post_photo_gallary.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:connectify/data/models/post/media_model.dart';
+
+class PostPhotoGallary extends StatefulWidget {
+  final List<MediaModel> medias;
+  final int initialIndex;
+
+  const PostPhotoGallary({super.key, required this.medias, required this.initialIndex});
+
+  @override
+  State<PostPhotoGallary> createState() => _PostPhotoGallary();
+}
+
+class _PostPhotoGallary extends State<PostPhotoGallary> {
+  late PageController _pageController;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          PhotoViewGallery.builder(
+            itemCount: widget.medias.length,
+            pageController: _pageController,
+            builder: (context, index) {
+              return PhotoViewGalleryPageOptions(
+                imageProvider: NetworkImage(widget.medias[index].mediaUrl),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 2,
+              );
+            },
+            onPageChanged: (index) => setState(() => _currentIndex = index),
+            backgroundDecoration: BoxDecoration(color: Colors.black),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "${_currentIndex + 1} / ${widget.medias.length}",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
