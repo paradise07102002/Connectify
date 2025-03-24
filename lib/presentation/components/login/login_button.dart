894966fd5987a0b599cce9cdc71bc572e2d21b4c
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-24 16:28:14 +0700

    refactor LoginButton: Updated LoginButton to use onError and onSuccess callbacks.

--- lib/presentation/components/login/login_button.dart
+++ lib/presentation/components/login/login_button.dart
@@ -1,6 +1,8 @@
 import 'package:connectify/domain/controllers/auth/login_controller.dart';
+import 'package:connectify/presentation/screens/main_screen.dart';
 import 'package:connectify/presentation/widgets/buttons/primary_button.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 
 class LoginButton extends StatelessWidget {
@@ -24,6 +26,23 @@ class LoginButton extends StatelessWidget {
               context: context,
               email: emailController.text.trim(),
               password: passwordController.text,
+              onError: (message) {
+                Fluttertoast.showToast(
+                  msg: message,
+                  backgroundColor: Colors.red,
+                );
+              },
+              onSuccess: () {
+                Fluttertoast.showToast(
+                  msg: "Login Success",
+                  backgroundColor: Colors.green,
+                );
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainScreen()),
+                  (route) => false,
+                );
+              },
             );
           },
         );
