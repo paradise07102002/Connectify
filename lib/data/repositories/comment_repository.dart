d04a2021882803c064f018eb93fcc025b724d4ce
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-27 16:16:19 +0700

    Feat CommentRepository: Add getComments method

--- lib/data/repositories/comment_repository.dart
+++ lib/data/repositories/comment_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:connectify/data/models/comment/create_comment_request.dart';
+import 'package:connectify/data/models/comment/get_comment_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -29,5 +30,25 @@ class CommentRepository {
     }
 
     return false;
+  }
+
+  Future<List<GetCommentModel>> getComments(String postId) async {
+    final accessToken = await secureStorage.read(key: 'access_token');
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/posts/$postId/comments'),
+      headers: {'accept': '*/*', 'Authorization': 'Bearer $accessToken'},
+    );
+
+    try {
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = jsonDecode(response.body);
+        return jsonData.map((e) => GetCommentModel.fromJson(e)).toList();
+      } else {
+        throw Exception('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
   }
 }
