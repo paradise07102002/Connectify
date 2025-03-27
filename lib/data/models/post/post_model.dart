951cb5bf98ccce96846c381bc6e8ad8d401c81f5
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-27 16:33:17 +0700

    Refactor PostModels: Delete CommentModel

--- lib/data/models/post/post_model.dart
+++ lib/data/models/post/post_model.dart
@@ -1,4 +1,3 @@
-import 'package:connectify/data/models/post/comment_model.dart';
 import 'package:connectify/data/models/post/like_model.dart';
 import 'package:connectify/data/models/post/media_model.dart';
 import 'package:connectify/data/models/post/share_model.dart';
@@ -13,7 +12,6 @@ class PostModel {
   final String avatarUrl;
   final String fullName;
   final List<MediaModel> medias;
-  final List<CommentModel> comments;
   final List<LikeModel> likes;
   final List<ShareModel> shares;
 
@@ -27,7 +25,6 @@ class PostModel {
     required this.avatarUrl,
     required this.fullName,
     required this.medias,
-    required this.comments,
     required this.likes,
     required this.shares,
   });
@@ -45,10 +42,6 @@ class PostModel {
       fullName: json['fullName'],
       medias:
           (json['medias'] as List).map((e) => MediaModel.fromJson(e)).toList(),
-      comments:
-          (json['comments'] as List)
-              .map((e) => CommentModel.fromJson(e))
-              .toList(),
       likes: (json['likes'] as List).map((e) => LikeModel.fromJson(e)).toList(),
       shares:
           (json['shares'] as List).map((e) => ShareModel.fromJson(e)).toList(),
@@ -65,7 +58,6 @@ class PostModel {
     'avatarUrl': avatarUrl,
     'fullName': fullName,
     'medias': medias.map((e) => e.toJson()).toList(),
-    'comments': comments.map((e) => e.toJson()).toList(),
     'likes': likes.map((e) => e.toJson()).toList(),
     'shares': shares.map((e) => e.toJson()).toList(),
   };
